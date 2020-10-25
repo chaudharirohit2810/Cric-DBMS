16d4991d3258d82a01afbcb063051f64f7ef3aad
Chetas Borse <cmborse3005@gmail.com>
2020-10-25 22:16:31 +0530

    Frontend started

---
+++ src/components/AddLeague.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
+
+class AddLeague extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            modal: false,
+            league_name: '',
+            format: '',
+        }   
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal,
+            msg: null
+        })
+    }
+
+    handle1 = event => {
+        this.setState({
+            league_name : event.target.value
+        })
+    }
+
+    handle2 = event => {
+        this.setState({
+            format: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        alert(`${this.state.league_name} match created`)
+        this.setState({
+            modal: false,
+            league_name: '',
+            format: '',
+        })
+    }
+
+    render() {
+  return (
+    <div>
+        <div style={{"text-align": "right", "margin": "10px", "paddingBlockEnd": "10px"}}>
+            <Button color="danger" onClick={this.toggle}>Add League</Button>
+        </div>
+            <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                <ModalHeader>Add League</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Row>
+                                <Col xs="2"><Label>League: </Label></Col>
+                                <Col xs="10"><Input type="name" name="league_name" id="league_name" value={this.state.league_name} onChange={this.handle1} placeholder="Enter league name"/></Col>
+                            </Row>
+                        </FormGroup>
+                        <FormGroup>
+                            <Row>
+                                <Col xs="2"><Label>Format </Label></Col>
+                                <Col xs="10"><Input type="select" name="format" id="format" value={this.state.format} onChange={this.handle2} placeholder="Choose format type">
+                                <option>T20</option>
+                                <option>ODI</option>
+                                <option>Test</option>
+                                 </Input>   
+                                </Col>
+                            </Row>
+                        </FormGroup>
+
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                <Button color="success" onClick={this.handleSubmit}>Add</Button>
+                <Button color="danger" onClick={this.toggle}>Back</Button>
+                </ModalFooter>
+            </Modal>
+    </div>
+  );
+    }
+}
+
+export default AddLeague;
---
+++ src/components/LeaguePage.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react'
+import { Col, Row, Button } from 'reactstrap'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import AddLeague from './AddLeague'
+import League_Seasons from './League_Seasons'
+
+class LeaguePage extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            leagues : [
+                {
+                    League_name: "Indian Premier League",
+                    Format: "T20",
+                    Description: "League Played in India"
+                },
+                {
+                    League_name: "Big Bash League",
+                    Format: "T20",
+                    Description: "League Played in Australia"
+                },
+                {
+                    League_name: "Caribbean Premier League",
+                    Format: "T20",
+                    Description: "League Played in Caribbean Islands"
+                },
+                {
+                    League_name: "ODI World Cup",
+                    Format: "ODI",
+                    Description: "League Played All over the world"
+                },
+                {
+                    League_name: "Champions Trophy",
+                    Format: "ODI",
+                    Description: "League Played All over the world"
+                },
+                {
+                    League_name: "T20 World Cup",
+                    Format: "T20",
+                    Description: "League Played all over the world"
+                },
+            ]
+        }
+    }
+
+    render() {
+        const leaguess = this.state.leagues.map((lea, index) =>
+                <React.Fragment>
+                <Col lg="6" style={{"marginBottom": "10px"}}>
+                    <Row>
+                        <Col lg="1"></Col>
+                            <Col lg="10" className="match">
+                                <div style ={{"text-align": "center"}}>
+                                    logo
+                                </div>
+                                <div style ={{"text-align": "center"}}>
+                                    {`League Name: ${lea.League_name}`}
+                                </div>
+                                <div style ={{"text-align": "center"}}>
+                                    {`League Format: ${lea.Format}`}
+                                </div>
+
+                                <div style ={{"text-align": "center"}}>
+                                    {/* <Router>
+                                        <Link to="/Season">Browse</Link>
+                                        <Switch>
+                                            <Route path="/Season">
+                                                <League_Seasons/>
+                                            </Route>
+                                        </Switch>
+                                    </Router> */}
+                                    
+                                </div>
+
+                            </Col>
+                        <Col lg="1"></Col>
+                    </Row>
+                </Col>
+                {/* <Col lg="1"></Col> */}
+                </React.Fragment>
+            )
+
+        return(
+            <React.Fragment>
+                <Row>
+                <Col>
+                <div className="titles">Popular Leagues</div>
+                </Col>
+                <Col>
+                <AddLeague/>
+                </Col>
+                </Row>
+                
+                <Row>
+                    {leaguess}
+                </Row>
+            </React.Fragment>
+        )
+    }
+}
+
+export default LeaguePage
